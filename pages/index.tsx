bb1ca68c6874b7547565554e567bb179f5d401dd
pragyakar <pragyakar75@gmail.com>
2021-06-19 16:19:20 +0545

    :sparkles: Add service to fetch active ipos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,39 @@
-import PageHead from '../components/SiteHead'
+import { GetServerSidePropsResult } from "next";
+import { getActiveIPOs, IActiveIPO } from "../services/iporesult";
+import handleApiError from "../utils/handleApiError";
 
-const Home = () => {
+interface IHomeProps {
+  activeIpos: IActiveIPO[];
+}
+
+const Home = (props: IHomeProps) => {
+  const { activeIpos } = props;
+
   return (
     <div>
       <main>
         <p>Hamro IPO</p>
+        {activeIpos.map((ipo, index) => {
+          return <p key={index}>{ipo.name}</p>
+        })}
       </main>
     </div>
-  )
+  );
+};
+
+export const getServerSideProps = async (): Promise<GetServerSidePropsResult<IHomeProps>> => {
+  try {
+    const activeIpos = await getActiveIPOs();
+
+    return {
+      props: { activeIpos },
+    };
+  } catch (error) {
+    handleApiError(error);
+    return {
+      props: { activeIpos: [] },
+    };
+  }
 };
 
 export default Home;
---
+++ services/iporesult.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+interface IAPIReponse {
+  success: boolean;
+  body: IActiveIPO[];
+  message: string;
+}
+
+export interface IActiveIPO {
+  id: number;
+  name: string;
+  scrip: string;
+  isFileUploaded: boolean;
+}
+
+const dummyReponse = {
+  success: true,
+  message: "Dummy Company shares retrieved.",
+  body: [
+    { id: 1, name: "MAHILA LAGHUBITTA BITTIYA SANSTHA LTD.", scrip: "MLBSL", isFileUploaded: true },
+    { id: 2, name: "Sunrise Bluechip Fund", scrip: "SBCF", isFileUploaded: true },
+    { id: 3, name: "Jyoti Life Insurance Ltd", scrip: "JLIC", isFileUploaded: true },
+    { id: 4, name: "NIBL Samriddhi Fund -2", scrip: "NIBLSFUND", isFileUploaded: true },
+  ],
+};
+
+export const getActiveIPOs = async (): Promise<IActiveIPO[]> => {
+  const url = "https://iporesult.cdsc.com.np/result/companyShares/fileUploaded";
+
+  try {
+    const { data } = await axios.get(url);
+    return dummyReponse.body;
+    // return data.body;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
